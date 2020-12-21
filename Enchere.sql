c1c89b549d3c9d43bc87069bd81555289b5d1cd9
Alesque-29 <59953836+Alesque-29@users.noreply.github.com>
2020-12-21 12:50:41 +0100

    Add files via upload

---
+++ Enchere.sql
@@ -0,0 +1,6 @@
+INSERT INTO Enchere (id, idArticleVendu, dateEnchere, proposition, prixVente, encherisseur, etatEnchere) VALUES (1, 1, '2020-11-25 22:22:00', 3000, 3000, 'Alesque29', TRUE);
+INSERT INTO Enchere (id, idArticleVendu, dateEnchere, proposition, prixVente, encherisseur, etatEnchere) VALUES (2, 2, '2020-12-04 10:01:00', 350, 350, 'Guigui02', TRUE);
+INSERT INTO Enchere (id, idArticleVendu, dateEnchere, proposition, prixVente, encherisseur, etatEnchere) VALUES (3, 3, '2020-12-14 22:22:00', 410, 0, 'Alesque29', TRUE);
+INSERT INTO Enchere (id, idArticleVendu, dateEnchere, proposition, prixVente, encherisseur, etatEnchere) VALUES (4, 3, '2020-12-16 10:01:00', 450, 450, 'Guigui02', TRUE);
+INSERT INTO Enchere (id, idArticleVendu, dateEnchere, proposition, prixVente, encherisseur, etatEnchere) VALUES (5, 11, '2021-01-10 11:11:00', 85, 0, 'Alesque29', false);
+INSERT INTO Enchere (id, idArticleVendu, dateEnchere, proposition, prixVente, encherisseur, etatEnchere) VALUES (6, 11, '2021-01-11 12:21:00', 150, 0, 'Alesque29', false);
